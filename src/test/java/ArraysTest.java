b0502573e421e5206d7f9c8ebd19504d9552abf3
Roberto Ortega <bertoort@Roberto.coloradocentaltelecom.com>
2016-02-21 09:42:06 -0700

    basic array method tests set up

--- src/test/java/ArraysTest.java
+++ src/test/java/ArraysTest.java
@@ -6,20 +6,20 @@ import org.junit.Assert;
 
 public class ArraysTest {
 
-  @Test
-  public void ShouldReverse() {
-    Arrays arrays = new Arrays();
-    int[] numArray = {4, 8, 15, 16, 23, 42};
-    int[] reversedArray = {42, 23, 16, 15, 8, 4};
-    Assert.assertArrayEquals(reversedArray, arrays.reverse(numArray));
-  }
-
-  @Test
-  public void ShouldPush() {
-    Arrays arrays = new Arrays();
-    int[] numArray = {4, 8, 15, 16, 23, 42};
-    int[] pushedArray = {4, 8, 15, 16, 23, 42, 10};
-    Assert.assertArrayEquals(pushedArray, arrays.push(numArray, 10));
-  }
+  // @Test
+  // public void ShouldReverse() {
+  //   Arrays arrays = new Arrays();
+  //   int[] numArray = {4, 8, 15, 16, 23, 42};
+  //   int[] reversedArray = {42, 23, 16, 15, 8, 4};
+  //   Assert.assertArrayEquals(reversedArray, arrays.reverse(numArray));
+  // }
+  //
+  // @Test
+  // public void ShouldPush() {
+  //   Arrays arrays = new Arrays();
+  //   int[] numArray = {4, 8, 15, 16, 23, 42};
+  //   int[] pushedArray = {4, 8, 15, 16, 23, 42, 10};
+  //   Assert.assertArrayEquals(pushedArray, arrays.push(numArray, 10));
+  // }
 
 }
