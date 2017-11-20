19f63ec26e7ef3bb3a34b4d2ed8e2d88ca590cdf
Ryan Wittrup <ryan.wittrup@gmail.com>
2017-11-20 17:22:22 -0600

    initial commit: solves for array utilities methods

--- src/main/java/ArrayUtils.java
+++ src/main/java/ArrayUtils.java
@@ -1,25 +1,38 @@
 package main;
 
 public class ArrayUtils {
-
-  public static int[] reverse(int[] array) {
-    return array;
-  };
-
-  public static int[] push(int[] array, int num) {
-    return array;
-  };
-
-  public static void print(int[] array) {
-    System.out.print('[');
-    for(int i=0; i<array.length; i++){
-      if (i == array.length -1) {
-        System.out.print(array[i] + "]");
-      } else {
-        System.out.print(array[i] + ", ");
-      }
+    public static int[] reverse(int[] array) {
+        int [] reversedArray = new int [array.length];
+        int end = array.length - 1;
+        for (int i = 0; i <= end; i++) {
+            reversedArray[end - i] = array[i];
+        }
+        return reversedArray;
     }
-    System.out.print('\n');
-  };
+
+    public static int[] push(int[] array, int num) {
+        int [] pushedArray = new int[array.length + 1];
+        int end = array.length - 1;
+        for (int i = 0; i <= end; i++) {
+            pushedArray[i] = array[i];
+        }
+        pushedArray[array.length] = num;
+
+        array = pushedArray; // could return pushedArray instead for copy
+
+        return array;
+    }
+
+    public static void print(int[] array) {
+        System.out.print('[');
+        for(int i=0; i<array.length; i++){
+            if (i == array.length -1) {
+                System.out.print(array[i] + "]");
+            } else {
+                System.out.print(array[i] + ", ");
+            }
+        }
+        System.out.print('\n');
+    }
 
 }
--- src/test/java/ArrayUtilsTest.java
+++ src/test/java/ArrayUtilsTest.java
@@ -5,19 +5,19 @@ import org.junit.Test;
 import org.junit.Assert;
 
 public class ArrayUtilsTest {
-  //
-  // @Test
-  // public void ShouldReverse() {
-  //   int[] numArray = {4, 8, 15, 16, 23, 42};
-  //   int[] reversedArray = {42, 23, 16, 15, 8, 4};
-  //   Assert.assertArrayEquals(reversedArray, ArrayUtils.reverse(numArray));
-  // }
-  //
-  // @Test
-  // public void ShouldPush() {
-  //   int[] numArray = {4, 8, 15, 16, 23, 42};
-  //   int[] pushedArray = {4, 8, 15, 16, 23, 42, 10};
-  //   Assert.assertArrayEquals(pushedArray, ArrayUtils.push(numArray, 10));
-  // }
+
+   @Test
+   public void ShouldReverse() {
+     int[] numArray = {4, 8, 15, 16, 23, 42};
+     int[] reversedArray = {42, 23, 16, 15, 8, 4};
+     Assert.assertArrayEquals(reversedArray, ArrayUtils.reverse(numArray));
+   }
+
+   @Test
+   public void ShouldPush() {
+     int[] numArray = {4, 8, 15, 16, 23, 42};
+     int[] pushedArray = {4, 8, 15, 16, 23, 42, 10};
+     Assert.assertArrayEquals(pushedArray, ArrayUtils.push(numArray, 10));
+   }
 
 }
