ebdbb08970da9ca84009e234080bc0c3ff635d51
Ryan Wittrup <ryan.wittrup@gmail.com>
2017-11-21 21:37:14 -0500

    implements queue as extension of linked list

--- src/main/java/Queue.java
+++ src/main/java/Queue.java
@@ -1,25 +1,81 @@
 package main;
 
+import java.util.EmptyStackException;
+
 public class Queue {
+    // reference to the head node.
+    private Node head;
+    private Node tail;
 
   public boolean isEmpty() {
-    return false;
+      if (this.head == null) {
+          return true;
+      } else {
+          return false;
+      }
   }
 
   public void enqueue(int value) {
-
+      if (this.head == null) {
+          this.head = new Node(value);
+      } else {
+          Node temp = this.head;
+          while(temp.getNext() != null){
+              temp = temp.getNext();
+          }
+          temp.setNext(new Node(value));
+      }
   }
 
   public int getFront() {
-    return 0;
+      if (this.isEmpty()) {
+          throw new EmptyStackException();
+      }
+      return this.head.getData();
   }
 
   public int dequeue() {
-    return 0;
+      if (this.isEmpty()) {
+          throw new EmptyStackException();
+      }
+      Node dequeue = this.head;
+      this.head = this.head.getNext();
+      dequeue.setNext(null);
+      return dequeue.getData();
   }
 
   public void clear() {
-
+      this.head = null;
   }
+
+    private class Node {
+        Node next;
+        // data carried by this node (currently int, could be any other type).
+        int data;
+        // Node constructor
+        public Node(int _data) {
+            next = null;
+            data = _data;
+        }
+        // another Node constructor if we want to
+        // specify the node to point to.
+        public Node(int _data, Node _next) {
+            next = _next;
+            data = _data;
+        }
+        // these methods should be self-explanatory
+        public int getData() {
+            return data;
+        }
+        public void setData(int _data) {
+            data = _data;
+        }
+        public Node getNext() {
+            return next;
+        }
+        public void setNext(Node _next) {
+            next = _next;
+        }
+    }
 
 }
